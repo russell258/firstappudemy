b4654d1e2b5d4414306e981fef737335871429c3
Russell258 <duckie258@gmail.com>
2023-08-29 20:35:59 +0800

    object data and fetching event data

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent {
   //this is property binded to the [disabled] in html
   allowNewServer: boolean = false;
   serverCreationStatus = 'No server was created';
+  serverName="";
 
   // once page loads, will set the allowNewServer to true after 2 seconds
   constructor(){
@@ -25,6 +26,11 @@ export class ServersComponent {
 
   onCreateServer(){
     this.serverCreationStatus="Server was created";
+  }
+
+  //adding ($event) in the html side allows you to pass the data over to this argument here
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
